8724a1b3ad1fdffd0aebaed8529d67cb91597251
Shcherbakov Alexey <lovelessxt@gmail.com>
2019-04-04 13:41:04 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-function showMessage() {
-  alert( 'Привет всем присутствующим!' );
-}
-
 function changepic() {
-  var src = ($("#my_image").attr("src") === "/img/first.jpg") ? "/img/first.jpg" : "/img/second.jpg";
-  alert( src );
+  if ($("#my_image").attr("src") == "/img/first.jpg") {
+    var src = "/img/second.jpg";
+    $("#text").text("Cобачка2");
+  }
+  else
+  {
+    var src = "/img/first.jpg";
+    $("#text").text("Cобачка1");
+  }
   $("#my_image").attr("src", src);
 }
