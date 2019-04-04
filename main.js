f6f78f82259bcdc60aa8b6bdbb79f9ea953e586d
Shcherbakov Alexey <lovelessxt@gmail.com>
2019-04-04 12:56:27 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-<script>
-      alert('XSS')
-      var src = ($("#my_image").attr("src") === "/img/first.jpg")
-                    ? "/img/first.jpg" 
-                    : "/img/second.jpg";
-      $("#my_image").attr("src", src);
-</script>
+alert('XSS');
+var src = ($("#my_image").attr("src") === "/img/first.jpg") ? "/img/first.jpg" : "/img/second.jpg";
+$("#my_image").attr("src", src);
+
